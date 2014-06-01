3d397f8020f4d8ee3105c78f218a48ab4bf4afba
Carl Pulley <c.j.pulley@hud.ac.uk>
2014-06-01 18:42:17 +0100

    Bug fix to cookbook

--- cookbook/cluster/attributes/default.rb
+++ cookbook/cluster/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:cluster][:service] = "cluster"
 default[:cluster][:deploy_dir] = "/usr/local/share"
 default[:cluster][:port] = 2552
-default[:cluster][:boot_class] = "cakesolutions.example.ClusterNodeApplication"
+default[:cluster][:boot_class] = "cakesolutions.example.WorkerNode"
 default[:cluster][:uid] = "cluster"
 default[:cluster][:shell] = "/bin/false"
 default[:cluster][:role] = nil
